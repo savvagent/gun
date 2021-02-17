a23ebe0183bb2b209b186846fc649e93be749b38
Rob Hicks <rob@hixfamily.org>
2021-02-16 19:39:19 -0700

    configure rollup to build after install

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,45 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
+import glob from 'glob';
+import resolve from '@rollup/plugin-node-resolve';
+import { resolve as res } from 'path';
 
 const sea = "./sea/index.js";
 const gun = "./src/index.js";
+const libInputs = glob.sync(res(__dirname, 'lib', '**', '*.js'));
 
-export default [
-  {
-    input: sea,
-    plugins: [ resolve({preferBuiltins: false}), commonJs() ],
-    output: {
-      file: 'dist/sea.js',
-      format: 'es'
-    }
-  },
-  {
-    input: gun,
-    plugins: [ resolve({preferBuiltins: false}), commonJs() ],
-    output: {
-      file: 'dist/gun.js',
-      format: 'es'
-    }
-  }
+const plugins = [
+  resolve({preferBuiltins: false}), commonJs()
 ]
+
+const libEntries = libInputs.map(input => {
+  const [ fPath, name ] = input.match(/.+\/(.+)/);
+  const fileName = name;
+  return {
+    input,
+    plugins,
+    context: 'null',
+    moduleContext: 'null',
+    output: { file: `./dist/${fileName}`, format: 'es' }
+  };
+});
+
+const seaEntry = {
+  input: sea,
+  plugins,
+  output: {
+    file: 'dist/sea.js',
+    format: 'es'
+  }
+};
+
+
+
+const gunEntry = {
+  input: gun,
+  plugins,
+  output: {
+    file: 'dist/gun.js',
+    format: 'es'
+  }
+};
+export default [ seaEntry, gunEntry];
